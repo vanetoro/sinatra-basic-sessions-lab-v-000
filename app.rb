a78cd649a0665da9e871bbfb13764ec431c7bf2c
Vanessa Toro <vanetoro84@gmail.com>
2018-05-24 23:40:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
     params.each do |key, value|
       @session[key] = @session[value]
     end
-    @item = item
+    # @item = item
     binding.pry
     erb :checkout
   end

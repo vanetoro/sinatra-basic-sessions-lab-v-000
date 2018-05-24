30a646eb5ea706f16f81eb03b6877a3fa009de4e
Vanessa Toro <vanetoro84@gmail.com>
2018-05-24 23:21:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,8 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   configure do
-    enable :session
-    set :session_secret
+    enable :sessions
+    set :session_secret, 'secret'
   end
 
   get '/' do

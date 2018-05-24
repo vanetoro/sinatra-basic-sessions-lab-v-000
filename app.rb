d5ea46260d6a9251d8cc9092da72b86b7f3447a0
Vanessa Toro <vanetoro84@gmail.com>
2018-05-24 23:47:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,26 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do    
+    erb :index
+  end
+
+
+
+  post '/checkout' do
+    @session = session
+    params.each do |key, value|
+      @session[key] = value
+    end
+    @item = @session[:item]
+    # binding.pry
+    erb :checkout
+  end
+
 end
